b9a3e8d75724e5a31a3c0a574e05c0fefe03073e
ealekseev <Eugene Alekseev>
2014-03-21 10:11:48 +0400

    Add check for chef-solo search.

--- recipes/zonefiles.rb
+++ recipes/zonefiles.rb
@@ -1,44 +1,47 @@
-
-search(:zones).each do |zone|
-  next unless zone['zone_info']
-  unless zone['autodomain'].nil? || zone['autodomain'] == ''
-    search(:node, "domain:#{zone['autodomain']}").each do |host|
-      next if host['ipaddress'] == '' || host['ipaddress'].nil?
-      zone['zone_info']['records'].push( {
-        "name" => host['hostname'],
-        "type" => "A",
-        "ip" => host['ipaddress']
-      })
+if Chef::Config['solo'] && !node['bind']['allow_solo_search']
+  Chef::Log.warn('This recipe uses search. Chef Solo does not support search.')
+else
+  search(:zones).each do |zone|
+    next unless zone['zone_info']
+    unless zone['autodomain'].nil? || zone['autodomain'] == ''
+      search(:node, "domain:#{zone['autodomain']}").each do |host|
+        next if host['ipaddress'] == '' || host['ipaddress'].nil?
+        zone['zone_info']['records'].push( {
+          "name" => host['hostname'],
+          "type" => "A",
+          "ip" => host['ipaddress']
+        })
+      end
     end
-  end
 
-  template "#{node[:bind][:vardir]}/#{node[:bind][:zonetype]}/#{zone['domain']}" do
-    source "#{node[:bind][:vardir]}/templates/#{zone['domain']}.erb"
-    local true
-    owner "root"
-    group "root"
-    mode 0644
-    notifies :restart, resources(:service => "bind9")
-    variables({
-      :serial => Time.new.strftime("%Y%m%d%H%M%S")
-    })
-    action :nothing
-  end
+    template "#{node[:bind][:vardir]}/#{node[:bind][:zonetype]}/#{zone['domain']}" do
+      source "#{node[:bind][:vardir]}/templates/#{zone['domain']}.erb"
+      local true
+      owner "root"
+      group "root"
+      mode 0644
+      notifies :restart, resources(:service => "bind9")
+      variables({
+        :serial => Time.new.strftime("%Y%m%d%H%M%S")
+      })
+      action :nothing
+    end
 
-  template "#{node[:bind][:vardir]}/templates/#{zone['domain']}.erb" do
-    source "zonefile.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    variables({
-      :domain => zone['domain'],
-      :soa => zone['zone_info']['soa'],
-      :contact => zone['zone_info']['contact'],
-      :global_ttl => zone['zone_info']['global_ttl'],
-      :nameserver => zone['zone_info']['nameserver'],
-      :mail_exchange => zone['zone_info']['mail_exchange'],
-      :records => zone['zone_info']['records']
-    })
-    notifies :create, resources(:template => "#{node[:bind][:vardir]}/#{node[:bind][:zonetype]}/#{zone['domain']}"), :immediately
+    template "#{node[:bind][:vardir]}/templates/#{zone['domain']}.erb" do
+      source "zonefile.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables({
+        :domain => zone['domain'],
+        :soa => zone['zone_info']['soa'],
+        :contact => zone['zone_info']['contact'],
+        :global_ttl => zone['zone_info']['global_ttl'],
+        :nameserver => zone['zone_info']['nameserver'],
+        :mail_exchange => zone['zone_info']['mail_exchange'],
+        :records => zone['zone_info']['records']
+      })
+      notifies :create, resources(:template => "#{node[:bind][:vardir]}/#{node[:bind][:zonetype]}/#{zone['domain']}"), :immediately
+    end
   end
 end
