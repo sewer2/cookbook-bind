4bb5ee5d34083c302ef5d50f278beb7dacb2db4e
sewer <sewer@clodo.ru>
2015-11-24 18:20:09 +0300

    fix restart service bind

--- recipes/default.rb
+++ recipes/default.rb
@@ -140,5 +140,5 @@ service node['bind']['service_name'] do
   action [:enable, :start]
   subscribes :reload, resources("template[#{node['bind']['sysconfdir']}/#{node['bind']['options_file']}]",
                                 "template[#{node['bind']['conf_file']}]"), :delayed
-  only_if { ::File.exists?(node['bind']['options_file']) && ::File.exists?(node['bind']['conf_file']) }
+  only_if { ::File.exists?("#{node['bind']['sysconfdir']}/#{node['bind']['options_file']}") && ::File.exists?(node['bind']['conf_file']) }
 end
